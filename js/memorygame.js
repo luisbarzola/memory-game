857f2ff9de31dd08667974f7f2381b1820d6da9a
luisbarzola <lrbarzola@gmail.com>
2016-02-11 00:59:56 -0300

    Agrego timer. Falta dar la funcionalidad de fin de juego cuando termina el tiempo o completó todos los casilleros

--- js/memorygame.js
+++ js/memorygame.js
@@ -8,45 +8,68 @@
 
     window.MemoryGame = function (where) {
         this.where = where;
-        this.box = $(where);
     };
 
-    MemoryGame.prototype.init = function (timeLimit, repeatedImgs, sourcesImgs) {
-        this.timeLimit = timeLimit;
+    MemoryGame.prototype.init = function (repeatedImgs, sourcesImgs, time) {
+        this.timeLimit = time[0];
+        this.timeWhere = time[1];
         this.repeatedImgs = repeatedImgs;
         this.sourcesImgs = sourcesImgs;
     };
 
     MemoryGame.prototype.run = function(){
-
         this.boxOpened = "";
         this.imgOpened = "";
         this.counter = 0;
         this.imgFound = 0;
-
-
-
+        this.timeExpered = false;
+        this.timeDiff = this.timeLimit;
 
         // crea las cajas con las imágenes de ImgSources
         // da dos vueltas por cada imagen.
-
-        var source = this.where;
-        for (var y = 1; y <= this.repeatedImgs ; y++) {
-            $.each(this.sourcesImgs, function(i, val) {
-                $(source).append("<div id=card" + y + i + "><img src=" + val + " />");
-            });
-        }
+        createBoxes(this);
 
         // bindea la funcionalidad click  para cada caja con imágenes para que se
         // ejecute  la función OpenCard
         var base = this;
-        $(source + " div").click(function(){
+
+        $(this.where + " div").click(function(){
             openCard(base, this);
         });
 
-        // mezcla todas las imágenes recién creadas.
-        ShuffleImages(source);
+        // mezcla todas las imágenes
+        shuffleImages(this.where);
 
+        // instancia el contador
+        var display = document.querySelector(this.timeWhere);
+        this.timer = startTimer(this, display);
+
+        setInterval(function(){
+            if (base.timeDiff <= 0) {
+                clearInterval(base.timer);
+                base.timeExpered = true;
+            }
+        }, 1000);
+
+    };
+
+    MemoryGame.prototype.reset = function(){
+        shuffleImages(this.where);
+        $(this.where + " div img").hide();
+        $(this.where + " div").css("visibility", "visible");
+        this.counter = 0;
+        $("#success").remove();
+        $("#counter").html("" + this.counter);
+        this.boxOpened = "";
+        this.imgOpened = "";
+        this.imgFound = 0;
+        this.timeExpered = false;
+
+        clearInterval(this.timer);
+        var display = document.querySelector(this.timeWhere);
+        this.timer = startTimer(this, display);
+
+        return false;
     };
 
 
@@ -59,16 +82,16 @@
      * @param {number} MaxValue
      * @returns {number}
      */
-    function RandomFunction(MinValue, MaxValue) {
+    var randomFunction = function(MinValue, MaxValue) {
         return Math.round(Math.random() * (MinValue - MaxValue) + MaxValue);
-    }
+    };
 
     /**
      * Agarra las imágenes de cada caja y las mezcla.
      */
-    function ShuffleImages(Source) {
-        var ImgAll = $(Source).children();
-        var ImgThis = $(Source + " div:first-child");
+    var shuffleImages = function(source) {
+        var ImgAll = $(source).children();
+        var ImgThis = $(source + " div:first-child");
         var ImgArr = new Array();
 
         // obtiene toda las imágenes
@@ -77,11 +100,11 @@
             ImgThis = ImgThis.next();
         }
 
-        ImgThis = $(Source + " div:first-child");
+        ImgThis = $(source + " div:first-child");
 
         // re organiza las imágenes
         for (var z = 0; z < ImgAll.length; z++) {
-            var RandomNumber = RandomFunction(0, ImgArr.length - 1);
+            var RandomNumber = randomFunction(0, ImgArr.length - 1);
 
             $("#" + ImgThis.attr("id") + " img").attr("src", ImgArr[RandomNumber]);
             ImgArr.splice(RandomNumber, 1);
@@ -89,7 +112,18 @@
         }
     }
 
+    /**
+     * Abre la tarjeta pasada por parámetro.
+     *
+     * @param game
+     * @param element
+     */
     var openCard = function (game, element) {
+        //si el tiempo terminó sale
+        if (game.timeExpered) {
+            return false;
+        }
+
         var source = game.where;
         var id = $(element).attr("id");
 
@@ -156,7 +190,50 @@
         }
     };
 
+    /**
+     * Crea las cajas a partir de las imágenes pasadas.
+     *
+     * @param game
+     */
+    var createBoxes = function(game){
+        for (var y = 1; y <= game.repeatedImgs ; y++) {
+            $.each(game.sourcesImgs, function(i, val) {
+                $(game.where).append(
+                    "<div id=card" + y + i + "><img src=" + val + " />"
+                );
+            });
+        }
+    }
 
+    var startTimer = function(game, display) {
+        var start = Date.now(),
+            diff,
+            minutes,
+            seconds;
+
+        function timer() {
+            // get the number of seconds that have elapsed since
+            // startTimer() was called
+            diff = game.timeLimit - (((Date.now() - start) / 1000) | 0);
+
+            // does the same job as parseInt truncates the float
+            minutes = (diff / 60) | 0;
+            seconds = (diff % 60) | 0;
+
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            display.textContent = minutes + ":" + seconds;
+
+        };
+        // we don't want to wait a full second before the timer starts
+        timer();
+
+        return setInterval(function(){
+            timer();
+            game.timeDiff = diff;
+        }, 1000);
+    }
 
 })(window, document, jQuery);
 
